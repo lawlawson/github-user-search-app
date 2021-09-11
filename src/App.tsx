68b1cd73e5420e971655ebc7af80a7b7edc76235
Lawrence <lawrence.narh-lawson@boomin.com>
2021-09-12 00:36:14 +0100

    Added components folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className='App'>
       <h1>Github User Search App</h1>
+      <Header />
     </div>
   );
 }
