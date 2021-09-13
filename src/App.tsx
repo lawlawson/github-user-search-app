4dd79b744570706b35eac1bc98f03692c25127f5
Lawrence <lawrence.narh-lawson@boomin.com>
2021-09-13 23:50:51 +0100

    added assets folder and search input field

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './components/Header/Header';
+import Search from './components/Search/Search';
 
 function App() {
   return (
     <div className='App'>
       <h1>Github User Search App</h1>
       <Header />
+      <Search />
     </div>
   );
 }
--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import TextField from '@material-ui/core/TextField';
 
 const Search = () => {
-  return <div></div>;
+  return (
+    <form noValidate autoComplete='off'>
+      <TextField id='standard-basic' placeholder='Search Github username' />
+    </form>
+  );
 };
 
 export default Search;
