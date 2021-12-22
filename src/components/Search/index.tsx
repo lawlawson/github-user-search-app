4101fd03e7fc64adbb965ee784971861d5069e10
Lawrence Narh-Lawson <lawrence.narh-lawson@boomin.com>
2021-12-22 23:46:42 +0000

    Refactoring code removing unused components

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
 import TextField from '@material-ui/core/TextField';
+
+// import SearchIcon from '@mui/icons-material/Search';
 
 const Search = () => {
   return (
     <form noValidate autoComplete='off'>
+      {/* <SearchIcon /> */}
       <TextField id='standard-basic' placeholder='Search Github username' />
     </form>
   );
